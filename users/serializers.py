f50f026a9156c1ccd3d775062b1af397e0d7713a
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-26 16:18:30 +0300

    Add PaymentSerializer and payments to UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,21 @@
+from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 
-from .models import User
+from .models import User, Payment
 
 
 class UserSerializer(ModelSerializer):
+    payments = SerializerMethodField()
+
+    def get_payments(self, user):
+        return [payment.date_of_payment for payment in Payment.objects.filter(user=user)]
+
     class Meta:
         model = User
+        fields = ["username", "email", "phone_number", "avatar", "city", "payments"]
+
+
+class PaymentSerializer(ModelSerializer):
+    class Meta:
+        model = Payment
         fields = "__all__"
