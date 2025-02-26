ce92b1cdd37f255d32110612e00f9c10e8126e7f
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-26 13:21:46 +0300

    Add CourseDetailSerializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -14,6 +14,21 @@ class CourseSerializer(ModelSerializer):
         fields = ["title", "image", "description", "lesson_count"]
 
 
+class CourseDetailSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+    lessons = SerializerMethodField()
+
+    def get_lessons(self, course):
+        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ["title", "image", "description", "lesson_count", "lessons"]
+
+
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
