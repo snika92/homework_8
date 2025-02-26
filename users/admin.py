ea4467cc6fbe49f56e61681f8708fd026303f142
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-26 12:53:11 +0300

    Add Payment to admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import Payment, User
 
 admin.site.register(User, UserAdmin)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "date_of_payment",
+        "course",
+        "lesson",
+        "payment_amount",
+        "method_of_payment",
+    )
+    search_fields = (
+        "user",
+        "course",
+        "lesson",
+        "payment_amount",
+    )
+    list_filter = (
+        "user",
+        "date_of_payment",
+        "course",
+        "lesson",
+        "method_of_payment",
+    )
