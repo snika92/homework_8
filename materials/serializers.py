fe53dfa06ed078ae5a75f37ad5f0349bf18a41e4
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-26 12:50:05 +0300

    Add field lessons_count to Course

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,12 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+
+    def get_lesson_count(self, obj):
+        return obj.lessons.count()
+
     class Meta:
         model = Course
-        fields = "__all__"
+        fields = ["title", "image", "description", "lesson_count"]
 
 
 class LessonSerializer(ModelSerializer):
