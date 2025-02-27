8ff23eb889821381aa8b16f7bd1a1e056431ebf5
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-27 14:41:15 +0300

    Fix UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,18 +4,15 @@ from rest_framework.serializers import ModelSerializer
 from .models import User, Payment
 
 
-class UserSerializer(ModelSerializer):
-    payments = SerializerMethodField()
-
-    def get_payments(self, user):
-        return [payment.date_of_payment for payment in Payment.objects.filter(user=user)]
-
-    class Meta:
-        model = User
-        fields = ["username", "email", "phone_number", "avatar", "city", "payments"]
-
-
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "phone_number", "avatar", "city", "payments"]
