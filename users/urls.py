fcd88e043e9542575885be955451776c8c886803
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-27 16:26:10 +0300

    Add urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import path
 from .apps import UsersConfig
-from .views import UserViewSet, PaymentListApiView
+from .views import UserViewSet, PaymentListApiView, UserCreateAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
@@ -9,7 +13,10 @@ router = SimpleRouter()
 router.register("", UserViewSet)
 
 urlpatterns = [
-path("payments/", PaymentListApiView.as_view(), name="payments_list"),
+    path('payments/', PaymentListApiView.as_view(), name='payments_list'),
+    path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
