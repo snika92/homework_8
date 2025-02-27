43ad8da5215a673047fb73aed1d4675b9ee8432b
Veronika Surkova <surkova_nika92@mail.ru>
2025-02-27 16:26:52 +0300

    Add UserCreateAPIView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import OrderingFilter
 
@@ -9,6 +9,16 @@ from .serializers import UserSerializer, PaymentSerializer
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class PaymentListApiView(ListAPIView):
