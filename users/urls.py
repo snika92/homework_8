717057d5a8a1501b1543690eb9278363151dc9ad
Veronika Surkova <surkova_nika92@mail.ru>
2025-03-04 18:29:56 +0300

    Fix urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import SimpleRouter
 from django.urls import path
 from .apps import UsersConfig
-from .views import UserViewSet, PaymentListApiView, UserCreateAPIView
+from .views import PaymentListApiView, UserCreateAPIView, UserListApiView, UserRetrieveApiView, UserUpdateApiView, UserDestroyApiView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -9,14 +10,19 @@ from rest_framework_simplejwt.views import (
 
 app_name = UsersConfig.name
 
-router = SimpleRouter()
-router.register("", UserViewSet)
+# router = SimpleRouter()
+# router.register("", UserViewSet)
 
 urlpatterns = [
     path('payments/', PaymentListApiView.as_view(), name='payments_list'),
+
+    path('', UserListApiView.as_view(), name='users_list'),
+    path('<int:pk>/', UserRetrieveApiView.as_view(), name='user_retrieve'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('<int:pk>/update/', UserUpdateApiView.as_view(), name="user_update"),
+    path('<int:pk>/delete/', UserDestroyApiView.as_view(), name="user_delete"),
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
