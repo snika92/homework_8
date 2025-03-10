c85563937d872742b48c7688c3b77714703585e6
Veronika Surkova <surkova_nika92@mail.ru>
2025-03-10 17:33:54 +0300

    Add model Subscription

--- materials/models.py
+++ materials/models.py
@@ -44,3 +44,18 @@ class Lesson(models.Model):
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
         ordering = ["title"]
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="subscriptions",
+                             verbose_name="Пользователь")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="subscriptions",
+                               verbose_name="Курс")
+
+    def __str__(self):
+        return f"{self.user} - {self.course}"
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        unique_together = ["user", "course"]
