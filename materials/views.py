15a1894aa6ca63bf0745d9b27699ac2408833c2d
Veronika Surkova <surkova_nika92@mail.ru>
2025-03-10 17:35:57 +0300

    Add SubscriptionAPIView

--- materials/views.py
+++ materials/views.py
@@ -3,12 +3,14 @@ from rest_framework.generics import (
     DestroyAPIView,
     ListAPIView,
     RetrieveAPIView,
-    UpdateAPIView,
+    UpdateAPIView, get_object_or_404,
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
 from materials.serializers import CourseSerializer, CourseDetailSerializer, LessonSerializer
 from users.permissions import IsModerator, IsOwner
 
@@ -68,3 +70,21 @@ class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
+
+
+class SubscriptionAPIView(APIView):
+
+    def post(self, *args, **kwargs):
+        user = self.request.user
+        course_id = self.request.data.get("course")
+        course_item = get_object_or_404(Course, pk=course_id)
+
+        subs_item = Subscription.objects.filter(user=user, course=course_item)
+
+        if subs_item.exists():
+            subs_item.delete()
+            message = 'Подписка удалена'
+        else:
+            subs_item.create(user=user, course=course_item)
+            message = 'Подписка добавлена'
+        return Response({"message": message})
