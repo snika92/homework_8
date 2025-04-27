1cb2b18df9017d803962af20abc271dcad0b48c0
Veronika Surkova <surkova_nika92@mail.ru>
2025-04-28 02:17:04 +0300

    Fix settings

--- config/settings.py
+++ config/settings.py
@@ -110,14 +110,14 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("NAME"),
-        "USER": os.getenv("USER"),
-        "PASSWORD": os.getenv("PASSWORD"),
-        "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT", default="5432"),
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT", default="5432"),
     }
 }
-
+print(f"DB CONNECTION: host={os.getenv('POSTGRES_HOST')}, port={os.getenv('POSTGRES_PORT')}")
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
